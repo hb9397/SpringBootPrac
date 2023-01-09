bb65da95a27f381d35ec9836641cee7674ce4175
hb9397@naver.com <hb9397@naver.com>
2023-01-09 15:55:20 +0900

    SpringBootPrac 20230109 - 2 Spring View - jsp, templateengine(thymeleaf)

---
+++ springview/src/main/java/com/kakao/springview/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.kakao.springview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", "Spring Boot 에서의 JSP");
+        return "main";
+    }
+}
