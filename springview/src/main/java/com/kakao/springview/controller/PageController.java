e3351271e5a2e509f161a824b9c173e757d52d65
hb9397@naver.com <hb9397@naver.com>
2023-01-09 17:54:13 +0900

    SpringBootPrac 20230109 - 3 Spring View templateengine(thymeleaf) DTO(SampleVO) 의 데이터를 Stream을 사용해서 LIST 로 전환 후 출력 출력에는 thymeleaf 의 반복문, 분기문 사용

--- springview/src/main/java/com/kakao/springview/controller/PageController.java
+++ springview/src/main/java/com/kakao/springview/controller/PageController.java
@@ -1,15 +1,19 @@
 package com.kakao.springview.controller;
 
+import com.kakao.springview.domain.SampleVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class PageController {
@@ -47,5 +51,20 @@ public class PageController {
     // 반환형이 void 면 출력하는 뷰이름은 요청 URL에 해당 되므로 출력할 view 파일은 ex1
     public void ex1(Model model){
         LOGGER.info("ex1 요청");
+    }
+
+    @GetMapping("/ex2")
+    public void ex2(Model model){
+        List<SampleVO> list = IntStream.range(1, 20).asLongStream().mapToObj(
+                i -> {
+                    SampleVO vo = SampleVO.builder()
+                            .sno(i)
+                            .first("First.." + i)
+                            .last("Last.." + i)
+                            .regTime(LocalDateTime.now())
+                            .build();
+                    return vo;
+                }).collect(Collectors.toList());
+        model.addAttribute("list", list);
     }
 }
