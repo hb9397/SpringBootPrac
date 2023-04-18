b622e06ef09458943cbf7af1087df91a3fbbadae
hb9397@naver.com <hb9397@naver.com>
2023-04-18 09:09:41 +0900

    kafka 로 JSON 전송하기 Consumer

---
+++ KafkaJSON_LOG_Consumer/src/main/java/com/kakao/kafkajson_log_consumer/KafkaJsonLogConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.kafkajson_log_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJsonLogConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJsonLogConsumerApplication.class, args);
+	}
+
+}
